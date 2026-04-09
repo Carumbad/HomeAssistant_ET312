0d4421499a4f336e230f787fd0210d81a47e51eb
Carumbad <Carumbad@hotmail.com>
2026-04-09 12:39:49 +0200

    Ignore local reference material

--- References/buttshock/__init__.py
+++
@@ -1,5 +0,0 @@
-from .errors import ButtshockChecksumError, ButtshockError, ButtshockIOError
-
-__version__ = '0.2.1'
-
-VERSION = __version__
--- References/buttshock/et312/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import ET312Base
-from .comm import ET312SerialSync
-from .emulator import ET312EmulatorSync
